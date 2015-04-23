9f8acdf4d244b9134a96a3f1a8657dc7633293a3
Shaun Abram <sabram@lendingclub.com>
2015-04-23 13:34:29 -0700

    updated welcome message in SampleController - added bootstart link.

--- src/main/resources/archetype-resources/src/main/java/SampleController.java
+++ src/main/resources/archetype-resources/src/main/java/SampleController.java
@@ -12,9 +12,10 @@ public class SampleController {
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
-		return "<h2>Welcome to $artifactId!<h2>" +
+		return "<h2>Welcome to $artifactId!</h2>" +
 				"\n" +
-				"Your bootstart generated project has started successfully";
+				"Your " +
+				"<a href=\"https://github.com/sabram/bootstart\">bootstart</a> generated project has started successfully";
 	}
 
 	public static void main(String[] args) throws Exception {
